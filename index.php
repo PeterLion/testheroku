717dba402ece8f14294da31dc3ce7b935cf34d75
Pierre Celestin NSHIMYUMUREMYI <nshimiyepeter@gmail.com>
2019-02-22 00:52:17 -0600

    Adjusted Nav menu width to be centered

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 </head>
 <body>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <div class="container">
+  	<a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -25,6 +26,7 @@
         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
       </li>
     </ul>
+  </div>
   </div>
 </nav>
 	<div class="container">
